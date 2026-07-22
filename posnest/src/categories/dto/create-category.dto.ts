54f7351640220a38e6c7665d66c1b68e8ecabc42
LuisD <cusiluis04@gmail.com>
2026-07-22 17:44:35 -0500

    first commit

---
+++ posnest/src/categories/dto/create-category.dto.ts
@@ -0,0 +1,6 @@
+import {IsNotEmpty, IsString} from 'class-validator'
+
+export class CreateCategoryDto {
+    @IsNotEmpty({message: 'El nombre de la categoria no puede ir vacio'})
+    name!: string 
+}
---
+++ posnest/src/categories/entities/category.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Category {
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column({type: 'varchar', length: 60})
+    name!: string
+
+
+
+}
