85f2cf1703f0fb20caf4900821a0a801cd928534
LuisD <cusiluis04@gmail.com>
2026-07-25 23:02:52 -0500

    feat: complete crud - transaction

---
+++ posnest/src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,40 @@
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "../../products/entities/product.entity";
+
+
+@Entity()
+export class Transaction {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column('decimal')
+    total!: number
+
+    @Column({type: 'timestamp', default: () => "CURRENT_TIMESTAMP(6)"})
+    transactionDate!: Date
+
+    @OneToMany(() =>  TransactionContents , (transaction) => transaction.transaction)
+    contents!: TransactionContents[]
+}
+
+@Entity()
+export class TransactionContents {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+    
+    @Column('int')
+    quantity!: number
+
+    @Column('decimal')
+    price!: number
+
+    @ManyToOne(() => Product, (product) => product.id, {eager: true})
+    product!: Product
+
+    @ManyToOne(() => Transaction, (transaction) => transaction.contents)
+    transaction!: Transaction
+
+
+}
---
+++ posnest/src/transactions/transactions.service.ts
@@ -0,0 +1,121 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTransactionDto } from './dto/create-transaction.dto';
+import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Transaction, TransactionContents } from './entities/transaction.entity';
+import { Between, FindManyOptions, Repository } from 'typeorm';
+import { Product } from '../products/entities/product.entity';
+import { endOfDay, isValid, parseISO, startOfDay } from 'date-fns';
+
+@Injectable()
+export class TransactionsService {
+  constructor(
+    @InjectRepository(Transaction) private readonly transactionRepository: Repository<Transaction>,
+    @InjectRepository(TransactionContents) private readonly transactionContentRepository: Repository<TransactionContents>,
+    @InjectRepository(Product) private readonly productRepository: Repository<Product>
+  ) { }
+
+  async create(createTransactionDto: CreateTransactionDto) {
+
+    await this.productRepository.manager.transaction(async (transactionEntityManager) => {
+      const transaction = new Transaction()
+      transaction.total = createTransactionDto.contents.reduce((total, item) => total + (item.quantity * item.price), 0)
+
+      for (const contents of createTransactionDto.contents) {
+        const product = await transactionEntityManager.findOneBy(Product, { id: contents.productId })
+
+        const errors: string[] = []
+
+        if (!product) { 
+          errors.push(`El producto con ID: ${contents.productId} no existe`)
+          throw new NotFoundException(errors);
+        }
+
+        if (contents.quantity > product.inventory) {
+          errors.push(`El articulo ${product.name} excede la cantidad disponible`)
+          throw new BadRequestException(errors)
+        }
+
+        product.inventory -= contents.quantity
+
+        // Create transaccionContent instance
+        const transaccionContent = new TransactionContents()
+        transaccionContent.quantity = contents.quantity
+        transaccionContent.price = contents.price
+        transaccionContent.product = product
+        transaccionContent.transaction = transaction
+
+        await transactionEntityManager.save(transaction)
+        await transactionEntityManager.save(transaccionContent)
+      }
+
+      
+    })
+
+    return "Venta almacenada correctamente."
+
+  }
+
+  findAll(transactionDate? : string) {
+    const options: FindManyOptions<Transaction> = {
+      relations : {
+        contents : true
+      }
+    }
+
+    if(transactionDate){
+      const date =  parseISO(transactionDate)
+      if(!isValid(date)){
+        throw new BadRequestException('Fecha no válida')
+      }
+
+      const start = startOfDay(date)
+      const end = endOfDay(date)
+
+      options.where ={
+        transactionDate: Between(start, end)
+      }
+    }
+
+    return this.transactionRepository.find(options)
+  }
+
+  async findOne(id: number) {
+    const transaction = await this.transactionRepository.findOne({
+      where: {
+        id
+      },
+      relations: {
+        contents: true
+      }
+    })
+
+    if(!transaction){
+      throw new NotFoundException('La transacción no existe')
+    }
+
+    return transaction
+  }
+
+  async remove(id: number) {
+    const transaction = await this.findOne(id)
+
+    for(const content of transaction.contents){
+      const product = await this.productRepository.findOneBy({id: content.product.id})
+      if(!product){
+         throw new NotFoundException('El producto no existe');
+      }
+      // Regresamos la cantidad de la venta cancelada
+      product.inventory += content.quantity
+
+      await this.productRepository.save(product)
+
+      const transaccionContent = await this.transactionContentRepository.findOneBy({id: content.id})
+      await this.transactionContentRepository.remove(transaccionContent!)
+    }
+
+    await this.transactionRepository.remove(transaction)
+    return {message: 'Venta eliminada correctamente'}
+  }
+}
+// REVISARRR EL CASCADE EN LA ENTIDAD TRANSACCION CONTENT
